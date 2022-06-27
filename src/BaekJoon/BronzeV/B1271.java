25383904042194a50e48f44f28096cf0a587ae6e
jieunee2 <980lje@naver.com>
2022-06-27 16:11:50 +0900

    엄청난 부자2 (1271)

---
+++ src/BaekJoon/BronzeV/B1271.java
@@ -0,0 +1,19 @@
+package BaekJoon.BronzeV;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B1271 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // BigInteger은 문자열 형태로 이루어져 있어 숫자의 범위가 무한하기에 어떠한 숫자이든지 담을 수 있음
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+
+    }
+}
