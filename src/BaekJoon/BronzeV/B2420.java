8bf29c4a87133c8ccfab85637e61b0777af43522
jieunee2 <980lje@naver.com>
2022-06-27 16:26:29 +0900

    #2420. 사파리월드

---
+++ src/BaekJoon/BronzeV/B2420.java
@@ -0,0 +1,14 @@
+package BaekJoon.BronzeV;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        System.out.println(a.subtract(b).abs());
+    }
+}
