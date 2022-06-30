dd342eee3e573480ca307df1a5cf2879ed815682
jieunee2 <980lje@naver.com>
2022-06-30 19:13:01 +0900

    #11654. 아스키코드

---
+++ src/BaekJoon/BronzeV/B11654.java
@@ -0,0 +1,18 @@
+package BaekJoon.BronzeV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+        int result = n.charAt(0);
+
+        System.out.println(result);
+
+    }
+
+}
