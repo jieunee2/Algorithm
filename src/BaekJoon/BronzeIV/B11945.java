77b5aeeeed1488afaa21194799507bc02abcd745
jieunee2 <980lje@naver.com>
2022-07-04 00:59:55 +0900

    #11945. 뜨거운 붕어빵

---
+++ src/BaekJoon/BronzeIV/B11945.java
@@ -0,0 +1,28 @@
+package BaekJoon.BronzeIV;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B11945 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder(br.readLine());
+            System.out.println(sb.reverse());
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
