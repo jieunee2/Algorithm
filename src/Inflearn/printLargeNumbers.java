30662551b22f88a8159f54757b11a646030c575f
jieunee2 <980lje@naver.com>
2022-07-05 20:40:58 +0900

    자바 알고리즘 문제풀이 - 1. 큰 수 출력하기

---
+++ src/Inflearn/printLargeNumbers.java
@@ -0,0 +1,41 @@
+package Inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class printLargeNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[n];
+        List<Integer> tmp = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+        tmp.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1])
+                tmp.add(arr[i]);
+        }
+
+        for (Integer i : tmp) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
