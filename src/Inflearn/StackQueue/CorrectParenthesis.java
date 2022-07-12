0e5965c38ff0a05a0d20e3a587ed41b42d3daf67
jieunee2 <980lje@naver.com>
2022-07-12 20:41:40 +0900

    자바 알고리즘 문제풀이 - 1. 올바른 괄호

--- src/Inflearn/StackQueue/CorrectParenthesis.java
+++ src/Inflearn/StackQueue/CorrectParenthesis.java
@@ -5,31 +5,40 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class CorrectParenthesis {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public String Solution(String str) {
+        String answer = "YES";
 
         Stack<Character> stack = new Stack<>();
 
-        String str = br.readLine();
-
         for (Character c : str.toCharArray()) {
-            if (c.equals('(')) {
+            if (c.equals('('))
                 stack.push(c);
-            } else {
-                int size = stack.size();
-                if (size == 0) {
-                    stack.push(c);
-                    break;
-                } else {
+            else {
+                // 스택이 비어있을 경우
+                if (stack.empty())
+                    return "NO";
+                // 스택이 비어있지 않을 경우
+                else
                     stack.pop();
-                }
             }
         }
 
-        if (stack.empty())
-            System.out.println("YES");
-        else
-            System.out.println("NO");
+        if (!stack.empty())
+            return "NO";
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        CorrectParenthesis c = new CorrectParenthesis();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+
+        System.out.println(c.Solution(str));
+
 
     }
 }
